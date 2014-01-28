ff02d1bfc14956b43d23732ecfccd0d068e8a393
Yilin Chen <wormful@gmail.com>
2014-01-28 16:32:42 +0800

    first commit

---
+++ lib/jquery/pjax/rails.rb
@@ -0,0 +1,9 @@
+require "jquery/pjax/rails/version"
+
+module Jquery
+  module Pjax
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jquery/pjax/rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Pjax
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
