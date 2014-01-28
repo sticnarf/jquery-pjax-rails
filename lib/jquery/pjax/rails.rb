46e7fff86efce2a4ba45f16b3e79a7cf05f610c6
Yilin Chen <wormful@gmail.com>
2014-01-28 16:59:13 +0800

    delete

--- lib/jquery/pjax/rails.rb
+++
@@ -1,9 +0,0 @@
-require "jquery/pjax/rails/version"
-
-module Jquery
-  module Pjax
-    module Rails
-      # Your code goes here...
-    end
-  end
-end
--- lib/jquery/pjax/rails/version.rb
+++
@@ -1,7 +0,0 @@
-module Jquery
-  module Pjax
-    module Rails
-      VERSION = "0.0.1"
-    end
-  end
-end
